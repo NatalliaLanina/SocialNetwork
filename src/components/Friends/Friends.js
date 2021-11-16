56bcdef2ebbb8fa6967f5ca243ecd08606e54089
Natallia Lanina <natalielanina@yahoo.com>
2021-11-16 14:44:00 +0300

    feat: add class Friends

--- src/components/Friends/Friends.js
+++ src/components/Friends/Friends.js
@@ -1,8 +1,47 @@
 import friendsClass from './Friends.module.css';
 import React from "react";
 import * as axios from "axios";
+import userPhoto from './user.png';
 
-const Friends = (props) => {
+class Friends extends React.Component {
+  componentDidMount() {
+    axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+      this.props.setFriends(response.data.items);
+    });
+  }
+
+  render() {
+    return <div>
+      {this.props.friendsData.map(user => <div key={user.id} className={friendsClass.main}>
+        <div className={friendsClass.avatar}>
+          <div>
+            <img src={user.photos.small !==null ? user.photos.small : userPhoto} alt="111"/>
+          </div>
+          <div>{user.followed ? <button onClick={() => {
+            this.props.unfollow(user.id)
+          }}>Unfollow</button> : <button onClick={() => {
+            this.props.follow(user.id)
+          }}>Follow</button>}</div>
+        </div>
+        <div className={friendsClass.item}>
+          <div>
+            <div className={friendsClass.name}>{user.name}</div>
+            <div>{user.status}</div>
+          </div>
+          <div>
+            <div>{'user.location.country'}</div>
+            <div>{'user.location.city'}</div>
+          </div>
+        </div>
+      </div>)}
+    </div>
+  }
+}
+
+export default Friends;
+
+
+/*const Friends = (props) => {
   const getFriends = () => {
     if (props.friendsData.length === 0) {
       axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
@@ -36,6 +75,4 @@ const Friends = (props) => {
       </div>
     </div>)}
   </div>
-};
-
-export default Friends;
+};*/
