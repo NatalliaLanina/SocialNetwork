d9c9a7bd58e09b1c82260c6279087e829ad7171d
Natallia Lanina <natalielanina@yahoo.com>
2021-11-26 13:39:54 +0300

    refactor: change main info appearance

--- src/components/Main/MainInfo/MainInfo.js
+++ src/components/Main/MainInfo/MainInfo.js
@@ -9,19 +9,21 @@ function MainInfo(props) {
 
   return (
     <div className={mainInfoClass.main__logo}>
-      {/*<div>
-        <img src="https://cdn-icons-png.flaticon.com/512/5149/5149017.png" alt="img"/>
-      </div>*/}
-
       <div>
-        <h3>{props.profile.fullName}</h3>
         <img src={props.profile.photos.small} alt="1111"/>
-        <p>{props.profile.status}</p>
+        <h3>{props.profile.fullName}</h3>
+        <i>
+          <p>{props.profile.aboutMe}</p>
+          <p>{props.profile.lookingForAJobDescription}</p>
+        </i>
+        <p>{props.profile.contacts.github}</p>
       </div>
     </div>
-
-
   );
 }
 
 export default MainInfo;
+
+/*<div>
+        <img src="https://cdn-icons-png.flaticon.com/512/5149/5149017.png" alt="img"/>
+      </div>*/
